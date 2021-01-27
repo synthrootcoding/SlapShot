2ab5f308c04449811443128aa593d0da3cbfb2ff
synthrootcoding <synthrootcoding@gmail.com>
2021-01-27 15:19:20 -0500

    connected api

---
+++ SlapShot/Models/HockeyDAL.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SlapShot.Models
+{
+    public class HockeyDAL
+    {
+        public static string CallLeagueStandingsAPI()
+        {
+            string url = $"https://api-hockey.p.rapidapi.com/standings/?league=19&season=2020";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            request.Headers.Add("x-rapidapi-key", "7cb9277a3emsh6e1c4215102d801p100293jsn7a4fd6481cb1");
+            request.Headers.Add("x-rapidapi-host", "api-hockey.p.rapidapi.com");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string output = rd.ReadToEnd();
+            return output;
+        }
+
+        public static LeagueStandings GetLeagueStandings()
+        {
+            string data = CallLeagueStandingsAPI();
+            LeagueStandings n = JsonConvert.DeserializeObject<LeagueStandings>(data);
+            return n;
+        }
+    }
+}
---
+++ SlapShot/Models/LeagueStandings.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlapShot.Models
+{
+
+    public class LeagueStandings
+    {
+        public string get { get; set; }
+        public Parameters parameters { get; set; }
+        public object[] errors { get; set; }
+        public int results { get; set; }
+        public Response[][] response { get; set; }
+    }
+
+    public class Parameters
+    {
+        public string league { get; set; }
+        public string season { get; set; }
+    }
+
+    public class Response
+    {
+        public int position { get; set; }
+        public string stage { get; set; }
+        public Group group { get; set; }
+        public Team team { get; set; }
+        public League league { get; set; }
+        public Country country { get; set; }
+        public Games games { get; set; }
+        public Goals goals { get; set; }
+        public int points { get; set; }
+        public string form { get; set; }
+        public string description { get; set; }
+    }
+
+    public class Group
+    {
+        public string name { get; set; }
+    }
+
+    public class Team
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string logo { get; set; }
+    }
+
+    public class League
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string logo { get; set; }
+        public int season { get; set; }
+    }
+
+    public class Country
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string code { get; set; }
+        public string flag { get; set; }
+    }
+
+    public class Games
+    {
+        public int played { get; set; }
+        public Win win { get; set; }
+        public Win_Overtime win_overtime { get; set; }
+        public Lose lose { get; set; }
+        public Lose_Overtime lose_overtime { get; set; }
+    }
+
+    public class Win
+    {
+        public int total { get; set; }
+        public string percentage { get; set; }
+    }
+
+    public class Win_Overtime
+    {
+        public int total { get; set; }
+        public string percentage { get; set; }
+    }
+
+    public class Lose
+    {
+        public int total { get; set; }
+        public string percentage { get; set; }
+    }
+
+    public class Lose_Overtime
+    {
+        public int total { get; set; }
+        public string percentage { get; set; }
+    }
+
+    public class Goals
+    {
+        public int _for { get; set; }
+        public int against { get; set; }
+    }
+
+}
